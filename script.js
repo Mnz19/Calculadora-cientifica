0cda6fb9edfe0f97abb20c95f0e14907633bcfd4
Thiago Menezes <thimenezes19@gmail.com>
2023-09-27 16:22:56 -0300

    Calculadora pronta

---
+++ script.js
@@ -0,0 +1,83 @@
+let displayValue = '';
+
+function adicionarDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function limparDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calcularResultado() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function squareRoot() {
+    try {
+        displayValue = Math.sqrt(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function square() {
+    try {
+        displayValue = Math.pow(eval(displayValue), 2);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function factorial() {z
+    try {
+        const num = parseInt(eval(displayValue));
+        let result = 1;
+        if (num >= 0) {
+            for (let i = 2; i <= num; i++) {
+                result *= i;
+            }
+            displayValue = result;
+            document.getElementById('display').value = displayValue;
+        } else {
+            document.getElementById('display').value = 'Erro';
+        }
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function logBase2() {
+    try {
+        displayValue = Math.log2(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function logBase10() {
+    try {
+        displayValue = Math.log10(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function naturalLog() {
+    try {
+        displayValue = Math.log(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
